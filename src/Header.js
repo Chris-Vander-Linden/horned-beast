b682d86054e79113dee8a8e621b3a0a4c72b46a8
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-09 14:07:55 -0600

    Update App.css
    Center containers and add color

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,11 @@ import React from "react";
 class Header extends React.Component {
 
   render() {
-    return <h1>Horned Beast</h1>;
+    return (
+      <header>
+        <h1>Horned Beast</h1>
+      </header>
+    );
   }
 }
 
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -9,11 +9,11 @@ class HornedBeast extends React.Component {
   }
 
   render() {
-    return <>
+    return <div className="beastContainer">
     <h2>{this.props.title}</h2>
     <img src={this.props.imageUrl} alt={this.props.title} title={this.props.title} />
     <p>{this.props.description}</p>
-    </>;
+    </div>;
   }
 }
 
