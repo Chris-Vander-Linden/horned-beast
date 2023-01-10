fc7549e52cb2a4e3720717be2499ab55f67a18eb
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-09 19:46:08 -0600

    Refactor App.js to class component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+import React from "react";
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
-
-
-
 import './App.css';
 
-function App() {
-  return (
-    <>
-    <Header />
-    <Main />
-    <Footer />
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return <>
+      <Header />
+      <Main />
+      <Footer />
+    </>;
+  }
 }
 
 export default App;
