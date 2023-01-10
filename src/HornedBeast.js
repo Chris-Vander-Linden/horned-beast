e0f99cba738e2bdaae81074a12256339c16bc1ad
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-09 23:54:56 -0600

    Add data.json

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,19 +1,24 @@
 import React from "react";
 
 class HornedBeast extends React.Component {
-  // I don't need to do this, since I'm not overriding the constructor.
   constructor(props){
     super(props);
 
-    this.state = {}
+    this.state = {likes: 0}
+  }
+
+  handleLikeClick = () => {
+    console.log("click");
+    this.setState({likes: this.state.likes + 1});
   }
 
   render() {
-    return <div className="beastContainer">
-    <h2>{this.props.title}</h2>
-    <img src={this.props.imageUrl} alt={this.props.title} title={this.props.title} />
-    <p>{this.props.description}</p>
-    </div>;
+      return <div className="beastContainer" key={this.props.id}>
+      <h2>{this.props.title}</h2>
+      <img onClick={this.handleLikeClick} src={this.props.imageUrl} alt={this.props.title} title={this.props.title} />
+      <p>&#9829; {this.state.likes}</p>
+      <p>{this.props.description}</p>
+      </div>;
   }
 }
 
