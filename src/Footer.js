8176524df37325d7d86578a4f0a2ab15683fb419
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-11 10:07:19 -0600

    Clean up styles
    I broked apart the App.css into modules that would match the components

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
+import './Footer.css';
 
 class Footer extends React.Component {
-  
   render() {
     return <footer>&copy;  {new Date().getFullYear()} Chris Vander Linden</footer>;
   }
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,30 +1,30 @@
 import React from "react";
+import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-
-    this.state = {likes: 0}
+    this.state = { likes: 0 }
   }
 
   handleLikeClick = () => {
     console.log("click");
-    this.setState({likes: this.state.likes + 1});
+    this.setState({ likes: this.state.likes + 1 });
   }
 
   render() {
-      return <div className="beastContainer" key={this.props.id}>
-        <h2>{this.props.title}</h2>
-        <div className="heart-container">
-          <div className="heart"><div>&#9829;</div><div>{this.state.likes}</div></div>
-          <div className="img-container">
-            <img onClick={this.handleLikeClick} src={this.props.imageUrl} alt={this.props.title} title={this.props.title} />
-          </div>
+    return <div className="beastContainer" key={this.props.id}>
+      <h2>{this.props.title}</h2>
+      <div className="heartContainer">
+        <div className="heart"><div>&#9829;</div><div>{this.state.likes}</div></div>
+        <div className="img-container">
+          <img onClick={this.handleLikeClick} src={this.props.imageUrl} alt={this.props.title} title={this.props.title} />
         </div>
-        <div className="beast-description-container">
-        <p className="beast-description">{this.props.description}</p>
-        </div>
-      </div>;
+      </div>
+      <div className="beastDescription-container">
+        <p className="beastDescription">{this.props.description}</p>
+      </div>
+    </div>;
   }
 }
 
