2f38ffa621504b1454f25f5160b965215cf4fc9a
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-12 14:45:28 -0600

    Update Modal Theme

--- src/SelectedBeast.js
+++ src/SelectedBeast.js
@@ -11,10 +11,10 @@ class SelectedBeast extends React.Component {
       <>
         <Modal show={this.props.show} fullscreen='lg-down' onHide={this.props.onClick}>
           <Modal.Header closeButton>
-            <Modal.Title>{this.props?.selectedBeast[0]?.title ?? ''} = {this.props?.likes !== 0 && this.props.likes + ' X '} {this.props?.likes !== 0 ? <Heart/> : <Heartbreak />}
+            <Modal.Title>{this.props?.selectedBeast[0]?.title ?? ''} = {this.props?.likes !== 0 && this.props.likes + ` x `} {this.props?.likes !== 0 ? <Heart /> : <Heartbreak />}
             </Modal.Title>
           </Modal.Header>
-          <Modal.Body>
+          <Modal.Body className={"bg-dark text-white"} >
             <img src={this.props?.selectedBeast[0]?.image_url ?? ''} alt='' /><br />
             {this.props?.selectedBeast[0]?.description ?? ''}
           </Modal.Body>
