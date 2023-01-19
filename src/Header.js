2e1128b77e8703cf5e33158e8f3a86abbfa8e294
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-19 12:38:54 -0600

    Add Header dropdown menu name search, horn filter, and sorts

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,57 @@
 import React from "react";
-//import { ToggleOn, ToggleOff } from 'react-bootstrap-icons';
-import { HeartPulse, HeartPulseFill } from 'react-bootstrap-icons';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import { ToggleOn, ToggleOff, HeartPulse, HeartPulseFill } from 'react-bootstrap-icons';
 import './Header.css';
 
 class Header extends React.Component {
   render() {
     const Pulse = this.props.pulse ? HeartPulse : HeartPulseFill;
-    //const Toggle = !this.props.darkTheme ? ToggleOn : ToggleOff;
+    const Toggle = !this.props.menu ? ToggleOn : ToggleOff;
+
     return (
       <header>
         <div id="headerOuterContainer">
-          <h1>Horned Beast</h1>
+          <h1><a href="./" >Horned Beast</a></h1>
           <div id="headerInnerContainer">
-            {/* Search Component Start */}
-            <label htmlFor="beastSearch"></label>
-            <input id="beastSearch" type="search" name="q" placeholder="Search Horned Beasts..." onKeyUp={this.props.eventHandlers.onKeyUp} />
-            {/* Search Component End */}
-            <Pulse width="32" height="32" onClick={() => { this.props.eventHandlers.handlePulseClick() }} />
-            {/* 
-            <Toggle width="40" height="40" onClick={() => { this.props.eventHandlers.handleThemeClick() }} />
-            */}
+            <Pulse id="menuHeart" width="32" height="32" onClick={this.props.eventHandlers.handlePulseClick} />
+            <Toggle width="40" height="40" onClick={this.props.eventHandlers.handleMenuClick} />
           </div>
+        </div>
+        <div id="menu" className={this.props.menu ? 'hideMenu' : 'showMenu'}>
+          <Form>
+            <InputGroup>
+              <Form.Control
+                placeholder="Search Horned Beasts..."
+                aria-label="Search Horned Beasts"
+                onKeyUp={this.props.eventHandlers.onKeyUp}
+              />
+
+              <Button name="sortAlphaDown" value={this.props.sortAlphaDown} variant="outline-secondary" onClick={this.props.onClick}>{this.props.sortAlphaDown === '' ? 'Sort?' : this.props.sortAlphaDown ? '\u2193' : '\u2191'}</Button>
+
+            </InputGroup>
+            <InputGroup>
+              <Form.Select aria-label="Number of horns" onChange={this.props.eventHandlers.handleSelectChange}>
+                <option value="">Min. # of horns</option>
+                { // build unique horn number options for form
+                  this.props.hornedBeastsData.map(beast => {
+                    // return array of horns
+                    return beast.horns;
+                  }).filter((beastHornNumber, index, array) => {
+                    // return only the first instance of horn number
+                    return array.indexOf(beastHornNumber) === index;
+                  }).map(horn => {
+                    // return html formatted array
+                    return <option key={horn} value={horn}> {horn}</option>
+                  })
+                }
+              </Form.Select>
+
+              <Button name="sortNumbersDown" value={this.props.sortNumbersDown} variant="outline-secondary" onClick={this.props.onClick}>{this.props.sortNumbersDown === '' ? 'Sort?' : this.props.sortNumbersDown ? '\u2193' : '\u2191'}</Button>
+
+            </InputGroup>
+          </Form>
         </div>
       </header>
     );
